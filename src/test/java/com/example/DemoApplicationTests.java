5653717ed71d692ee99f07bca88b4ac06844db5c
mikeechen <mikeechen915@gmail.com>
2017-02-27 13:24:37 -0800

    changed something

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -13,11 +13,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Random;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,6 +53,26 @@ public class DemoApplicationTests {
 				.andExpect(jsonPath("$.bandName", is("One Republic")));
 
 		verify(this.repo).save(any(Album.class));
+	}
+
+	@Test
+	public void getTest() throws Exception {
+		Long id = new Random().nextLong();
+		Album album = new Album();
+		album.setName("Waking Up");
+		album.setBandName("One Republic");
+		album.setId(id);
+
+		when(this.repo.findAll()).thenReturn(Collections.singletonList(album));
+
+		MockHttpServletRequestBuilder req = MockMvcRequestBuilders.get("/album")
+				.contentType(MediaType.APPLICATION_JSON);
+
+		this.mvc.perform(req)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id", is(id)))
+				.andExpect(jsonPath("$[0].name", is("Waking Up")))
+				.andExpect(jsonPath("$[0].bandName", is("One Republic")));
 	}
 
 }
